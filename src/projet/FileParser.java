e0168810ac9d1c5727ec64b59f45c83d149ed618
Fatima Zahra <fatiscence@gmail.com>
2012-10-12 11:30:03 +0200

    Redaction de la documentation du code

--- src/projet/FileParser.java
+++ src/projet/FileParser.java
@@ -1,3 +1,11 @@
+/*!
+ *  \author Noufal FARAH & Fatiam Zahra AGNI 
+ *  \version 1.0.0
+ *  \since 11/10/2012 
+ *  \package projet
+ *  \brief ce package contient les classes ContentHandler.java et FileParser.java  
+ * 
+ */
 package projet;
 
 import java.io.File;
@@ -5,9 +13,23 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
+
+
+/*!
+ * \class la classe FileParser contenant la methode parse 
+ * 
+ */
 public class FileParser {
-	
-	
+	 
+	 /*!
+	  * \brief cette méthode permet de lire le contenu d'un fichier et d'afficher le nombre de lignes qu'il contient 
+	  *  
+	  *  La classe FileParser correspond au lecteur, elle comporte une méthode # parse() qui prend en paramètre le fichier à lire et un handler permettant
+	  *  d'éxecuter des actions lors de la lecture de ce fichier (callbacks) 
+	  *  \param file le fichier a lire 
+	  *  \param handler pour exécuter les actions lors de la lecture
+	  *  \throws FileNotFoundException l'exception de fichier non trouvé 
+	  */
 	public void parse(File f,ContentHandler handler) throws FileNotFoundException
 	{
 		int temp=0;
